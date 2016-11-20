b18eeb4b1b7dbc142771a16306cc9b488001ea31
javadsabbagh <j.sabbagh89@gmail.com>
2016-11-21 02:38:55 +0330

    Create a basic template for the application

--- desktop/WorkflowDesigner/mainwindow.cpp
+++ desktop/WorkflowDesigner/mainwindow.cpp
@@ -6,6 +6,12 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // FIXME correct dimension based on scene view
+    scene = new QGraphicsScene(0, 0, 800, 600);
+    ui->graphicsView->setScene(scene);
+
+    scene->addRect(0, 0, 100, 200, QPen(Qt::darkRed), QBrush(Qt::red, Qt::FDiagPattern));
 }
 
 MainWindow::~MainWindow()
